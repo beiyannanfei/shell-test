8305856578d040a57a3fb260327003e33362e845
wangyanqiang <wangyanqiang@sensoro.com>
2017-04-19 11:09:34 +0800

    update

--- SHELL/03-删除过期文件/my4linux.sh
+++ SHELL/03-删除过期文件/my4linux.sh
@@ -4,13 +4,13 @@ sleep 10s   #延时10秒
 #删除n天前的*.log文件
 n=$((10*86400))     #保留10天
 today=$(date +%Y%m%d)
-todayStamp=`date -j -f "%Y%m%d" "$today" +%s`
+todayStamp=`date -d "${today} +%s"`
 echo "today is ${today} timeStamp is ${todayStamp}"
 for file in *.log
 do
 #    echo $file
     fileDate=${file:0:8}    #提取文件前八位字符获取日期
-    fileDateStamp=`date -j -f "%Y%m%d" "$fileDate" +%s`
+    fileDateStamp=`date -d "${fileDate} +%s"`
     secDiff=`expr ${todayStamp} - ${fileDateStamp}`
 #    echo "${fileDate} timeStamp is ${fileDateStamp} diff of ${today} is ${secDiff}"
     if [ ${secDiff} -ge ${n} ]; then
