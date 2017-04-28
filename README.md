e72410208884338ddf7c9ebaeb86bae3f8ecdbf4
wangyanqiang <wangyanqiang@sensoro.com>
2017-04-28 11:37:12 +0800

    update

--- README.md
+++
@@ -1,8 +0,0 @@
-shell command test
-所有shell脚本都是在mac系统测试并运行
-移植到linux系统后在centOS系统进行测试
-
-
-
-
-
---
+++ SHELL/04-压缩文件&MD5/my.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+rm -rf *.gz *.log
+counter=0
+while [ ${counter} -lt 10 ]
+do
+    fileName="test-${counter}.log"
+    counter=`expr ${counter} + 1`
+    if [ ! -f $fileName ]; then
+        touch ${fileName}
+        echo "create file ${fileName} finish"
+    fi
+done
+today=`date +%Y%m%d`
+tarFileName="test-${today}.tar.gz"
+tar -zcvf ${tarFileName} *.log
+echo "tar ${tarFileName} finish"
+baseFileName=${tarFileName%%.*}
+echo "base file name ${baseFileName}"
+md5Str=MD5 ${baseFileName}
+echo "${baseFileName} MD5 value is ${md5Str}"
+newFileName="${baseFileName}.${md5Str}.tar.gz"
+echo "newFileName is ${newFileName}"
+cp ${tarFileName} ${newFileName}
+
+
+
+
+
+
+
+
+
