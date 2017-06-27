c67a6646d0b16c41a5ea87ea84ff938e863239f9
wangyanqiang <wangyanqiang@sensoro.com>
2017-06-27 16:06:29 +0800

    update

---
+++ SHELL/08-select.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+select ch in "begin" "end" "exit"
+do
+    case ${ch} in
+        "begin")
+            echo "start something"
+            ;;
+        "end")
+            echo "stop something"
+            ;;
+        "exit")
+            echo "exit"
+            break;
+            ;;
+        *)
+            echo "Ignore"
+            ;;
+    esac
+done;
+
